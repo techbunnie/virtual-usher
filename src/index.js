14e7566c9a91846f19ff425d49c255c3224a41fe
techbunnie <huiee_low@yahoo.com>
2023-11-17 21:55:27 +0800

    add express.js into the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import express from "express";
 import * as pino from "pino";
 
 import * as rsvpHandler from "#alfred/handler/rest/rsvp.js";
@@ -6,6 +7,25 @@ import * as rsvpHandler from "#alfred/handler/rest/rsvp.js";
 const logger = pino.pino();
 
 
+const expressApp = express();
+const expressPort = 3000;
+
+expressApp.get("/", (request, response) => {
+    response.send("Hello World!");
+});
+
+expressApp.get("/registration", (request, response) => {
+    response.send("I am suppose to render registration.html here");
+});
+
+expressApp.get("/dashboard", (request, response) => {
+    response.send(JSON.stringify({
+        data: {
+            guestPercent: 87
+        }
+    }));
+});
+
 function main() {
     logger.info("hello world!");
 
@@ -13,3 +33,7 @@ function main() {
 }
 
 main();
+
+expressApp.listen(expressPort, () => {
+    logger.info(`example application now listening at port: ${expressPort}`);
+});
