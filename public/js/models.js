551752cf2ab8e82966f451f57f7959b721372fc7
techbunnie <huiee_low@yahoo.com>
2023-11-25 10:23:01 +0800

    add model to represent an event

--- public/js/models.js
+++ public/js/models.js
@@ -1,9 +1,58 @@
 /**
- * @property {string} id
- * @property {string} email
- * @property {Date} confirmedAt
- * @property {Date} checkedinAt
- * @property {Date} createdAt
+ * represents an event and its related information
+ *
+ * @property {String} id
+ * @property {Number} expectedGuests
+ * @property {Date}   createdAt
+ * @property {Date}   updatedAt
+ */
+export class Event {
+    id;
+    expectedGuests;
+    createdAt;
+    updatedAt;
+
+    constructor(data) {
+        if (typeof data == "object" && data != null) {
+            this.id = data["id"] || null;
+            this.expectedGuests = data["expectedGuests"] || null;
+
+            this.createdAt = data["createdAt"] || null;
+            this.createdAt = (this.createdAt !== null)
+                             ? new Date(this.createdAt)
+                             : null;
+
+            this.updatedAt = data["updatedAt"] || null;
+            this.updatedAt = (this.updatedAt !== null)
+                            ? new Date(this.updatedAt)
+                            : null;
+
+            return;
+        }
+
+        this.id = null;
+        this.expectedGuests = null;
+        this.createdAt = null;
+        this.updatedAt = null;
+    }
+
+    setExpectedGuests(value) {
+        if (value <= 0)
+            throw new Error("invalid expected guests value");
+
+        this.expectedGuests = value;
+    }
+}
+
+
+/**
+ * represents a guest entry for the event
+ *
+ * @property {String} id
+ * @property {String} email
+ * @property {Date}   confirmedAt
+ * @property {Date}   checkedinAt
+ * @property {Date}   createdAt
  */
 export class Guest {
     id;
